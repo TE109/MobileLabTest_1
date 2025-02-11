4f9d86c81ca6edc6196a4084865d7ece5f779f27
TE109 <1991tmce@gmail.com>
2025-02-10 19:27:12 -0500

    Testing Random Number Generator

--- Lab1_tomer_edelman/ContentView.swift
+++ Lab1_tomer_edelman/ContentView.swift
@@ -10,15 +10,18 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    @Query private var items: [Item]  // Query items from the model context
+    
     var body: some View {
-        Text("Select an item")
-    }
+        VStack {
+            Text("Random Number: \(Int.random(in: 1..<100))")  // Display the random number
+            
 
+        }
+    }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(for: Item.self, inMemory: true)  // For preview purposes, using in-memory storage
 }
